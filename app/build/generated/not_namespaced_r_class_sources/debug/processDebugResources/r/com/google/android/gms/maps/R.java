fc0d4b4376c2ff6384d03520e9e5cad8535f8cbf
Adrian T. Saycon <adzbite@gmail.com>
2019-06-10 17:35:57 +0800

    fix

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/maps/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/maps/R.java
@@ -40,11 +40,11 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int hybrid = 0x7f080057;
-        public static final int none = 0x7f08007b;
-        public static final int normal = 0x7f08007c;
-        public static final int satellite = 0x7f08008e;
-        public static final int terrain = 0x7f0800be;
+        public static final int hybrid = 0x7f080059;
+        public static final int none = 0x7f08007e;
+        public static final int normal = 0x7f08007f;
+        public static final int satellite = 0x7f080091;
+        public static final int terrain = 0x7f0800c1;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/adriansaycon/rbit_cartester/MainActivity.kt
+++ app/src/main/java/com/adriansaycon/rbit_cartester/MainActivity.kt
@@ -1,45 +1,91 @@
 package com.adriansaycon.rbit_cartester
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import android.view.Menu
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        testAdz("Is there something wrong?")
         setContentView(R.layout.activity_main)
-        val myToolbar = findViewById<Toolbar>(R.id.mainToolbar)
-        setSupportActionBar(myToolbar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val drawer = findViewById<DrawerLayout>(R.id.mainDrawer)
-        drawer.openDrawer(GravityCompat.START)
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
     }
 
-    fun onMenuItemClick(item: MenuItem): Boolean {
-        // Handle item selection
-        testAdz(item.itemId.toString())
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
         return true
-//        return when (item.itemId) {
-//            R.id.new_game -> {
-//                newGame()
-//                true
-//            }
-//            R.id.help -> {
-//                showHelp()
-//                true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
     }
 
-    fun testAdz(msg: String) {
-        Log.d("adz", msg)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_home -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_tools -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
